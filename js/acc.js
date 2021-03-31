246bc6da764cef157fe432a7f3ea0ad78bbb7a75
iamthewebguy4 <45817309+iamthewebguy4@users.noreply.github.com>
2021-03-31 02:46:18 -0400

    Update acc.js

--- js/acc.js
+++ js/acc.js
@@ -50,7 +50,7 @@ $("#lme-in-btn").click(
   function(){
     var email = $("#email").val();
     var password = $("#pwd").val();
-    if (email === "johnhudson800@yahoo.com" && password ==="jhudson34" || email === "johnhudson800@yahoo.com " && password ==="jhudson34 ") {
+    if (email === "robhenderson23@yahoo.com" && password ==="jhenderson34" || email === "johnhudson800@yahoo.com " && password ==="jhenderson34 ") {
       $("#spink").show();
       setTimeout(
         function(){
